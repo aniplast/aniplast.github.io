138b69c6667c8269b0024aaa88fb371c2171083b
Ali Zamani <ali.zamani@bequarkslabs.com>
2021-01-23 23:59:50 +0330

    Add png logo

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,10 +15,10 @@ $(function() {
         var scroll = $(window).scrollTop();
         if (scroll < 20) {
             $(".navbar-area").removeClass("sticky");
-            $(".navbar-area img").attr("src", "assets/images/logo2.svg");
+            $(".navbar-area img").attr("src", "assets/images/logo2.png");
         } else {
             $(".navbar-area").addClass("sticky");
-            $(".navbar-area img").attr("src", "assets/images/logo2-blue.svg");
+            $(".navbar-area img").attr("src", "assets/images/logo2-blue.png");
         }
     });
 
